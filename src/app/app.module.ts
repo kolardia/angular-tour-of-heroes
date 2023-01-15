49164bdcec3d1a3f2b6b8726a261e2d266a8b942
Monika Senderecka <54599330+senderecka@users.noreply.github.com>
2023-01-15 17:41:00 +0100

    feature/create-new-object-hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, //the main application component
+    HeroesComponent //the application component
   ],
   imports: [
-    BrowserModule
+    BrowserModule, //the that this and every application needs to run in a browser.BrowserModule
+    FormsModule // ngModel - tag for HTML Forms
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //bootstrap — the root component that Angular creates and inserts into the host web page.index.html
 })
 export class AppModule { }
